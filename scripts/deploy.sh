f715898c087166c7e0619b539da3bf497121accf
bjoberg <brett@obergmail.com>
2017-01-29 17:20:35 -0600

    Removed clutter from shell script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,6 @@ setup_git() {
   git config --global user.name "bjoberg"
 }
 
-# add_remote() {
-#   git remote add origin https://github.com/bjoberg/brettoberg.com.git
-# }
-
 push_to_master() {
     git config -l
     git push origin testing:master
@@ -14,7 +10,5 @@ push_to_master() {
 
 setup_git
 echo "Git configuration setup"
-# add_remote
-# echo "Remote has been added"
 push_to_master
 echo "Testing branch has been pushed to master"
