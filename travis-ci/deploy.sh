dd3a90db2cf381e72eb44815b9e6f3fedcff10d5
bjoberg <brett@obergmail.com>
2017-01-29 16:35:06 -0600

    Testing deploy shell script

--- travis-ci/deploy.sh
+++ travis-ci/deploy.sh
@@ -1,19 +1,19 @@
-setup_git() {
-  git config --global user.email "brett@obergmail.com"
-  git config --global user.name "bjoberg"
-}
+# setup_git() {
+#   git config --global user.email "brett@obergmail.com"
+#   git config --global user.name "bjoberg"
+# }
 
-add_remote() {
-  git remote add origin https://github.com/bjoberg/brettoberg.com.git
-}
+# add_remote() {
+#   git remote add origin https://github.com/bjoberg/brettoberg.com.git
+# }
 
 push_to_master() {
-    git push origin testing:master
+    git push origin development:testing
 }
 
-setup_git
-echo "Git configuration setup"
-add_remote
-echo "Remote has been added"
+# setup_git
+# echo "Git configuration setup"
+# add_remote
+# echo "Remote has been added"
 push_to_master
 echo "Testing branch has been pushed to master"
