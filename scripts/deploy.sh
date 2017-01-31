60ffa52ce4cbe7cddc76fa8f3dd20b94ed970f19
bjoberg <brett@obergmail.com>
2017-01-30 22:55:42 -0600

    Added Environment variable for token

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,10 @@
+clone_testing_branch() {
+  git clone https://$MY_OAUTH@github.com/bjoberg/brettoberg.com.git
+}
+
 setup_git() {
   git config --global user.email "brett@obergmail.com"
   git config --global user.name "bjoberg"
-  echo $MY_TEST
 }
 
 push_to_master() {
@@ -9,7 +12,9 @@ push_to_master() {
     git push origin testing:master
 }
 
+clone_testing_branch
+echo "Testing branch cloned."
 setup_git
-echo "Git configuration setup"
+echo "Git configuration setup."
 push_to_master
-echo "Testing branch has been pushed to master"
+echo "Testing branch has been pushed to master."
