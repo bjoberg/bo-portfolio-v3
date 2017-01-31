a7583bb94e7e4d0cf988631a84af55f4ad9cead0
bjoberg <brett@obergmail.com>
2017-01-31 08:59:04 -0600

    Reorganize deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,27 +1,26 @@
 clone_repo() {
   git clone https://$MY_OAUTH@github.com/bjoberg/brettoberg.com.git
   cd brettoberg.com
+  echo "Repo cloned. Moved into brettoberg.com directory."
 }
 
 checkout_testing_branch() {
   git checkout testing
+  echo "Testing branch checked out."
 }
 
 setup_git() {
   git config --global user.email "brett@obergmail.com"
   git config --global user.name "bjoberg"
+  echo "Git configuration setup."
 }
 
 push_to_master() {
-    git status
     git push origin testing:master
+    echo "Testing branch has been pushed to master."
 }
 
 clone_repo
-echo "Repo cloned. Moved into brettoberg.com directory."
 checkout_testing_branch
-echo "Testing branch checked out."
 setup_git
-echo "Git configuration setup."
 push_to_master
-echo "Testing branch has been pushed to master."
