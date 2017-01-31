20bd50725653e4efca454bc685fefb1f31b45563
bjoberg <brett@obergmail.com>
2017-01-30 23:01:23 -0600

    Still testing deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 clone_testing_branch() {
   git clone https://$MY_OAUTH@github.com/bjoberg/brettoberg.com.git
+  cd brettoberg.com
 }
 
 setup_git() {
@@ -13,7 +14,7 @@ push_to_master() {
 }
 
 clone_testing_branch
-echo "Testing branch cloned."
+echo "Testing branch cloned. Moved into brettoberg.com directory."
 setup_git
 echo "Git configuration setup."
 push_to_master
