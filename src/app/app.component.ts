48940e0b8850c43c2073ebda7983f358316ef307
bjoberg <brett@obergmail.com>
2017-02-27 10:10:50 -0600

    Fixed Instagram link
    Updated Instagram link from ../bjoberg/ to ../brett.oberg/ to reflect my username change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -44,7 +44,7 @@ export class AppComponent {
     // Add my extras
     this.extras = [new Extra("Resume", new URL("https://drive.google.com/file/d/0B4i30BX81iQGUldkdENTZXNucEk/view?usp=sharing"), "<i class='fa fa-file-text-o fa-lg' aria-hidden='true'></i>")];
     this.extras.push(new Extra("Github", new URL("https://github.com/bjoberg"), "<i class='fa fa-github fa-lg' aria-hidden='true'></i>"));
-    this.extras.push(new Extra("Instagram", new URL("https://www.instagram.com/bjoberg/"), "<i class='fa fa-instagram fa-lg' aria-hidden='true'></i>"));
+    this.extras.push(new Extra("Instagram", new URL("https://www.instagram.com/brett.oberg/"), "<i class='fa fa-instagram fa-lg' aria-hidden='true'></i>"));
     this.extras.push(new Extra("500px", new URL("https://500px.com/bjoberg"), "<i class='fa fa-500px fa-lg' aria-hidden='true'></i>"));
   }
 
