89d1950e083fb5c2da990f09cc2eccae4c703987
Brett Oberg <brett@obergmail.com>
2018-12-15 10:19:17 -0800

    Update to Angular 7
    - Update to Angular 7
    - Reorganize directory structure
    - Update packages

--- server.ts
+++ server.ts
@@ -2,11 +2,13 @@
 import 'zone.js/dist/zone-node';
 import 'reflect-metadata';
 
+import { renderModuleFactory } from '@angular/platform-server';
 import { enableProdMode } from '@angular/core';
 
 import * as express from 'express';
 import * as compression from 'compression';
 import { join } from 'path';
+import { readFileSync } from 'fs';
 
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
@@ -18,20 +20,30 @@ const PORT = process.env.PORT || 5000;
 const DIST_FOLDER = join(process.cwd(), 'dist');
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
-// * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/main.bundle');
+const template = readFileSync(join(DIST_FOLDER, 'browser', 'index.html')).toString();
 
-// Express Engine
-import { ngExpressEngine } from '@nguniversal/express-engine';
+// * NOTE :: leave this as require() since this file is built Dynamically from webpack
+const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/main');
+
 // Import module map for lazy loading
 import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 
-app.engine('html', ngExpressEngine({
-  bootstrap: AppServerModuleNgFactory,
-  providers: [
-    provideModuleMap(LAZY_MODULE_MAP)
-  ]
-}));
+app.engine('html', (_, options, callback) => {
+  renderModuleFactory(AppServerModuleNgFactory, {
+    // Our index.html
+    document: template,
+    url: options.req.url,
+    // DI so that we can get lazy-loading to work differently (since we need it to just instantly render it)
+    extraProviders: [
+      provideModuleMap(LAZY_MODULE_MAP)
+    ]
+  }).then(html => {
+    callback(null, html);
+  });
+});
+
+app.set('view engine', 'html');
+app.set('views', join(DIST_FOLDER, 'browser'));
 
 // Route to https
 function requireHttps(req, res, next) {
